52cf3af54979ec530cd912b68eb70470dbb313be
github username <github email address>
2019-08-22 11:31:07 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,52 +1,58 @@
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
+    register Sinatra::ActiveRecordExtension
+    set :views, Proc.new { File.join(root, "../views/") }
 
-  configure do
-    enable :sessions
-    set :session_secret, "secret"
-  end
-
-  get '/' do
-    erb :home
-  end
-
-  get '/registrations/signup' do
-
-    erb :'/registrations/signup'
-  end
-
-  post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
-
-    redirect '/users/home'
-  end
-
-  get '/sessions/login' do
-
-    # the line of code below render the view page in app/views/sessions/login.erb
-    erb :'sessions/login'
-  end
-
-  post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/users/home'
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
     end
-    redirect '/sessions/login'
-  end
 
-  get '/sessions/logout' do
-    session.clear
-    redirect '/'
-  end
+    # Kind of like an index page but now showing all users. 
+    get '/' do
+        erb :home
+    end
 
-  get '/users/home' do
+    # Kind of like a #new. Renders signup form.
+    get '/registrations/signup' do
+        erb :'/registrations/signup'
+    end
 
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
-  end
+    # Kind of line a #create. Post to new User.
+    post '/registrations' do
+        @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+        @user.save
+        session[:user_id] = @user.id
+
+        redirect '/users/home'
+    end
+
+    # Brings you to the login form.
+    get '/sessions/login' do
+
+        # the line of code below render the view page in app/views/sessions/login.erb
+        erb :'sessions/login'
+    end
+
+    # Login in user via the form by finding the user in database.
+    post '/sessions' do
+        @user = User.find_by(email: params[:email], password: params[:password])
+        if @user
+        session[:user_id] = @user.id
+        redirect '/users/home'
+        end
+        redirect '/sessions/login'
+    end
+
+    # Logs user out by clearing the session.
+    get '/sessions/logout' do
+        session.clear
+        redirect '/'
+    end
+
+
+    # Kind of like a #show page.
+    get '/users/home' do
+        @user = User.find(session[:user_id])
+        erb :'/users/home'
+    end
 end
